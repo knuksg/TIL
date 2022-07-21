9fe0f37d82d7dd0fc5606edf242e47dab3fb1616
knuksg <108652851+knuksg@users.noreply.github.com>
2022-07-21 21:23:03 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,9 +1,4 @@
-S = list(input())
-alphabet = {}
-for i in range(97, 123):
-    alphabet[i] = -1
-for i in S:
-    if alphabet[ord(i)] == -1:
-        alphabet[ord(i)] = S.index(i)
-for i in range(97, 123):
-    print(alphabet[i], end=' ')
+print(*map(input().find,map(chr,range(97,123))))
+# 97에서 122까지 chr 함수 적용한 것에다가
+# input().find 함수 적용한 것을 출력.
+# a.find(b)는 a 안에 b가 있을 경우 첫 번째 인덱스 출력, 없을 경우 -1 출력.
