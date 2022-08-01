506f006be58f460419c2009d6d9d9d803f498502
knuksg <108652851+knuksg@users.noreply.github.com>
2022-08-01 17:45:13 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 196 ms, Memory: 48632 KB -BaekjoonHub

---
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+member = {}
+for _ in range(n):
+    nn = input()
+    member[nn] = member.get(nn, 0) + 1
+for _ in range(n-1):
+    nn = input()
+    member[nn] -= 1
+print(sorted(member.items(), key=lambda x:x[1])[-1][0])
