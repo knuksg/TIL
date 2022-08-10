63b3079c8fc22ed494a1e204b3d75d84439d80fe
knuksg <108652851+knuksg@users.noreply.github.com>
2022-08-10 19:36:30 +0900

    [Silver II] Title: 섬의 개수, Time: 364 ms, Memory: 117940 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,14 +1,20 @@
+'''
+테스트 케이스
+5 4
+1 0 1 0 0
+1 0 0 0 0
+1 0 1 0 1
+1 0 0 1 0
+'''
 while True:
+    # 지도의 너비와 높이를 입력받는다.
     w, h = map(int, input().split())
-
+    # 마지막 줄에 0, 0이 들어오면 브레이크.
     if w == 0 and h == 0:
         break
-
+    # land에는 지도를 입력하고, visited에는 각 지점에 방문했는지를 확인하기 위해 초기화한다.
     land = [list(map(int, input().split())) for _ in range(h)]
     visited = [[False] * w for _ in range(h)]
-
-    dr = [0, 0, 1, -1, 1, -1, 1, -1]
-    dc = [1, -1, 0, 0, 1, -1, -1, 1]
 
     '''
     [[1, 0, 1, 0, 0], 
@@ -21,9 +27,12 @@ while True:
     [False, False, False, False, False]]
     '''
 
+    dr = [0, 0, 1, -1, 1, -1, 1, -1]
+    dc = [1, -1, 0, 0, 1, -1, -1, 1]
+
     stack = []
     dic_ = {}
-
+    # 각 지점을 순회한다. 이어진 지점의 정보를 딕셔너리에 넣는다.
     for r in range(h):
         for c in range(w):
             # 땅일 경우에만 시행.
@@ -32,19 +41,21 @@ while True:
             # 이미 온 땅이 아닐 경우에만 시행.
             if visited[r][c] == True:
                 continue
-
+            # 방문했다는 표시를 해주고, 딕셔너리에 추가한다.
             visited[r][c] = True
             dic_[(r, c)] = []
-
+            # 여덟 방위를 돌면서 현재 지점과 이어져있는지 확인한다.
             for d in range(8):
                 nr = r + dr[d]
                 nc = c + dc[d]
-            
+                # 조건 1. 지점이 범위를 벗어나면 안 된다.
                 if not (-1<nr<h and -1<nc<w):
                     continue
+                # 조건 2. 지점이 바다(0)이면 안 된다.
                 if not land[nr][nc]:
                     continue
-                dic_[(r, c)] = dic_.get((r, c), []) + [(nr, nc)]
+                # 기준 좌표를 키로 넣고, 이동한 좌표를 값으로 넣는다.
+                dic_[(r, c)] += [(nr, nc)]
 
     '''
     {(0, 0): [(1, 0)],
@@ -69,13 +80,11 @@ while True:
                 if adj not in visited: # 아직 방문하지 않았다면
                     visited.append(adj) # 방문 처리 
                     stack.append(adj) # 스택에 넣기
-    list_ = []
     cnt = 0
+    # 딕셔너리를 순회한다.
     for i in dic_:
-        if i not in list_:
+        # 키가 리스트에 없으면 dfs 함수를 사용한다.
+        if i not in visited:
             dfs(i)
             cnt += 1
-            for j in dic_:
-                if j in visited:
-                    list_.append(j)
     print(cnt)
