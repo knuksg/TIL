7fe747dc795062a7cec4b0ee3aa203ef21b2c321
knuksg <108652851+knuksg@users.noreply.github.com>
2022-08-12 17:51:51 +0900

    [Bronze I] Title: 이항 계수 1, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,12 @@
+from sys import stdin
+def factorial(n):
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+n, k = map(int, stdin.readline().split())
+print(factorial(n) // (factorial(k) * factorial(n - k)))
